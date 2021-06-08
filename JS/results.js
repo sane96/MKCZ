be5cf25afe03e630d698467a92273d37b92ea751
Danilo Santrac <danilosantrac96@gmail.com>
2021-06-08 17:36:10 +0200

    version 1.3

--- JS/results.js
+++ JS/results.js
@@ -1,5 +1,16 @@
-// 'use strict';
+'use strict';
 
-// const resultsOutline = document.querySelectorAll('.results-outline');
+const resultsOutline = document.querySelectorAll('.results-outline');
 
-// resultsOutline
+
+resultsOutline.forEach(result => result.addEventListener('click', function(e) {
+    
+    if(e.target.parentElement.parentElement.classList.value === 'column column-document') {
+        e.target.parentElement;
+    } else {
+
+    this.parentElement.lastElementChild.classList.toggle('results-dropdown-visible');
+    this.firstElementChild.firstElementChild.firstElementChild.classList.toggle('pointer-transformed');
+    }
+}))
+
