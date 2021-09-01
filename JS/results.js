cb09e4c2499622f61dc471e5eee29f7252ebc9b0
unknown <danilosantrac96@gmail.com>
2021-09-01 14:29:22 +0200

    fixed smooth reveal results

--- JS/results.js
+++ JS/results.js
@@ -2,7 +2,7 @@
 
 const resultsOutline = document.querySelectorAll('.results-outline');
 const selects = document.querySelectorAll('.select');
-const competitions = document.querySelectorAll('.results-single');
+const competitionsResults = document.querySelectorAll('.results-dropdown-single');
 
 
 // Open select tables
@@ -34,29 +34,35 @@ for (const option of document.querySelectorAll(".option")) {
 }
 
 
-selects.forEach(select => select.addEventListener('click', function(e) {
-    if(e.target.classList.contains('select-trigger') || e.target.parentElement.classList.contains('select-trigger')) return;
-    if(!(select.parentElement.firstElementChild.textContent === "Категорија")) return;
-    
-    competitions.forEach(competition => {
-        if(!(competition.firstElementChild.lastElementChild.previousElementSibling.textContent.trim() === e.target.textContent) && !(e.target.textContent === "Све")) competition.style.display = "none";
-        else competition.style.display = "flex";
-        return;
-    });
-}))
-
-const resultsDrops = [...document.querySelectorAll('.results-dropdown')];
-console.log(resultsDrops);
-const resultsNum = resultsDrops.map(drop => drop.children.length);
-console.log(resultsNum);
-
 // Opening result tabs
-resultsOutline.forEach(result => result.addEventListener('click', function(e) {
+let resultsDrop = [...document.querySelectorAll('.results-dropdown')].map(result => [...result.parentElement.lastElementChild.children].length);
+console.log(resultsDrop);
+
+
+
+resultsOutline.forEach((result,i) => result.addEventListener('click', function(e) {
+    // resultsDrop = [...result.parentElement.lastElementChild.children].length;
+
     if(e.target.parentElement.parentElement.classList.value === 'column column-document') return e.target.parentElement; 
     this.parentElement.lastElementChild.classList.toggle('results-dropdown-visible');
-    if(!this.parentElement.lastElementChild.style.height) {this.parentElement.lastElementChild.style.height = `${5 * 3}rem`}
+    if(!this.parentElement.lastElementChild.style.height) {this.parentElement.lastElementChild.style.height = `${4 * resultsDrop[i]}rem`}
     else this.parentElement.lastElementChild.style.height = '';
     this.firstElementChild.firstElementChild.firstElementChild.classList.toggle('pointer-transformed');
 }));
 
+
+// Filtering results for age category
+selects.forEach(select => select.addEventListener('click', function(e) {
+    
+    if(e.target.classList.contains('select-trigger') || e.target.parentElement.classList.contains('select-trigger')) return;
+    if(!(select.parentElement.firstElementChild.textContent === "Категорија")) return;
+    
+    competitionsResults.forEach(result => {
+        if(!(result.lastElementChild.previousElementSibling.textContent.trim() === e.target.textContent) && !(e.target.textContent === "Све")) result.style.display = "none";
+        else result.style.display = "flex";      
+    });
+
+    // resultsOutline.forEach((res,i) => res.parentElement.lastElementChild.style.height = `${4 * resultsDrop[i]}rem`)
+    
+}))
 
