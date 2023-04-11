53cc5f4f44de5447673fa39b93a0aa11e987489e
Danilo Santrac <81857475+sane96@users.noreply.github.com>
2023-04-11 02:18:24 +0200

    Result tables update

--- JS/results.js
+++ JS/results.js
@@ -5,64 +5,38 @@ const selects = document.querySelectorAll('.select');
 const competitionsResults = document.querySelectorAll('.results-dropdown-single');
 
 
-// Open select tables
 
-for (const dropdown of document.querySelectorAll(".select-wrapper")) {
-    dropdown.addEventListener('click', function() {
-        this.querySelector('.select').classList.toggle('open');
-    })
-}
-
-
-window.addEventListener('click', function(e) {
-    for (const select of document.querySelectorAll('.select')) {
-        if (!select.contains(e.target)) {
-            select.classList.remove('open');
-        }
-    }
-});
-
-
-for (const option of document.querySelectorAll(".option")) {
-    option.addEventListener('click', function() {
-        if (!this.classList.contains('selected')) {
-            this.parentNode.querySelector('.option.selected').classList.remove('selected');
-            this.classList.add('selected');
-            this.closest('.select').querySelector('.select-trigger span').textContent = this.textContent;
-        }
-    })
-}
 
 
 // Opening result tabs
-let resultsDrop = [...document.querySelectorAll('.results-dropdown')].map(result => [...result.parentElement.lastElementChild.children].length);
-console.log(resultsDrop);
+// let resultsDrop = [...document.querySelectorAll('.results-dropdown')].map(result => [...result.parentElement.lastElementChild.children].length);
+// console.log(resultsDrop);
 
 
 
-resultsOutline.forEach((result,i) => result.addEventListener('click', function(e) {
-    // resultsDrop = [...result.parentElement.lastElementChild.children].length;
+// resultsOutline.forEach((result,i) => result.addEventListener('click', function(e) {
+//     // resultsDrop = [...result.parentElement.lastElementChild.children].length;
 
-    if(e.target.parentElement.parentElement.classList.value === 'column column-document') return e.target.parentElement; 
-    this.parentElement.lastElementChild.classList.toggle('results-dropdown-visible');
-    if(!this.parentElement.lastElementChild.style.height) {this.parentElement.lastElementChild.style.height = `${4 * resultsDrop[i]}rem`}
-    else this.parentElement.lastElementChild.style.height = '';
-    this.firstElementChild.firstElementChild.firstElementChild.classList.toggle('pointer-transformed');
-}));
+//     if(e.target.parentElement.parentElement.classList.value === 'column column-document') return e.target.parentElement; 
+//     this.parentElement.lastElementChild.classList.toggle('results-dropdown-visible');
+//     if(!this.parentElement.lastElementChild.style.height) {this.parentElement.lastElementChild.style.height = `${4 * resultsDrop[i]}rem`}
+//     else this.parentElement.lastElementChild.style.height = '';
+//     this.firstElementChild.firstElementChild.firstElementChild.classList.toggle('pointer-transformed');
+// }));
 
 
 // Filtering results for age category
-selects.forEach(select => select.addEventListener('click', function(e) {
+// selects.forEach(select => select.addEventListener('click', function(e) {
     
-    if(e.target.classList.contains('select-trigger') || e.target.parentElement.classList.contains('select-trigger')) return;
-    if(!(select.parentElement.firstElementChild.textContent === "Категорија")) return;
+//     if(e.target.classList.contains('select-trigger') || e.target.parentElement.classList.contains('select-trigger')) return;
+//     if(!(select.parentElement.firstElementChild.textContent === "Категорија")) return;
     
-    competitionsResults.forEach(result => {
-        if(!(result.lastElementChild.previousElementSibling.textContent.trim() === e.target.textContent) && !(e.target.textContent === "Све")) result.style.display = "none";
-        else result.style.display = "flex";      
-    });
+//     competitionsResults.forEach(result => {
+//         if(!(result.lastElementChild.previousElementSibling.textContent.trim() === e.target.textContent) && !(e.target.textContent === "Све")) result.style.display = "none";
+//         else result.style.display = "flex";      
+//     });
 
-    // resultsOutline.forEach((res,i) => res.parentElement.lastElementChild.style.height = `${4 * resultsDrop[i]}rem`)
+//     // resultsOutline.forEach((res,i) => res.parentElement.lastElementChild.style.height = `${4 * resultsDrop[i]}rem`)
     
-}))
+// }))
 
--- JS/script.js
+++ JS/script.js
@@ -1,37 +1,41 @@
 'use strict';
 
-const contentElements = [...document.querySelector('.content').children];
+const contentElements = [...document.querySelector('.container-content').children];
 
-const pageId = document.body.id;
-let verificationObj = {
-    home: true,
-    news: true, 
-    results: true, 
-    administration: true,
-    coaches: true,
-    history: true,
-    contact: true, 
-    single: true,
-};
+const filterIcons = [...document.querySelectorAll('.icon-filters')];
 
-// Make session
+const dropdownFilters = [...document.querySelectorAll('.dropdown-filters')];
 
-if(!sessionStorage.initObj) {
-    sessionStorage.setItem('initObj', JSON.stringify(verificationObj));
-}
+// const pageId = document.body.id;
+// let verificationObj = {
+//     home: true,
+//     news: true, 
+//     results: true, 
+//     administration: true,
+//     coaches: true,
+//     history: true,
+//     contact: true, 
+//     single: true,
+// };
+
+// // Make session
+
+// if(!sessionStorage.initObj) {
+//     sessionStorage.setItem('initObj', JSON.stringify(verificationObj));
+// }
 
 
-const verificationId = JSON.parse(sessionStorage.getItem('initObj'))[pageId];
+// const verificationId = JSON.parse(sessionStorage.getItem('initObj'))[pageId];
 
 
 
-if(verificationId) {
-    let parsedObj = JSON.parse(sessionStorage.getItem('initObj'));
+// if(verificationId) {
+//     let parsedObj = JSON.parse(sessionStorage.getItem('initObj'));
 
-    parsedObj[pageId] = false;
-    sessionStorage.setItem('initObj', JSON.stringify(parsedObj));
+//     parsedObj[pageId] = false;
+//     sessionStorage.setItem('initObj', JSON.stringify(parsedObj));
 
-// contentElements.forEach(el => el.style.setProperty ("transition", "transform 1s, opacity 1s"));
+contentElements.forEach(el => el.style.setProperty ("transition", "transform 1s, opacity 1s"));
 contentElements.forEach(el => el.classList.add('element-hidden'));
 
 const elementTransition = window.getComputedStyle(contentElements[0]).transition;
@@ -78,5 +82,46 @@ contentElements.forEach(function(ele) {
     contentObserver.observe(ele);
        
 });
-};
+// };
+
+// Open filter dropdown
+filterIcons.forEach(icon => icon.addEventListener('click', function (e) {
+    if(e.target.parentElement.classList.contains('icon-filters')) {
+        e.target.parentElement.nextElementSibling.classList.toggle('dropdown-filters-active');
+        e.target.parentElement.classList.toggle('icon-filters-active');
+    }
+
+    if(e.target.parentElement.classList.contains('filters-row')) {
+        e.target.nextElementSibling.classList.toggle('dropdown-filters-active');
+        e.target.classList.toggle('icon-filters-active');
+    }
+}))
+
+// Open select filter
+
+for (const dropdown of document.querySelectorAll(".select-wrapper")) {
+    dropdown.addEventListener('click', function() {
+        this.querySelector('.select').classList.toggle('open');
+    })
+}
+
+
+window.addEventListener('click', function(e) {
+    for (const select of document.querySelectorAll('.select')) {
+        if (!select.contains(e.target)) {
+            select.classList.remove('open');
+        }
+    }
+});
+
+
+for (const option of document.querySelectorAll(".option")) {
+    option.addEventListener('click', function() {
+        if (!this.classList.contains('selected')) {
+            this.parentNode.querySelector('.option.selected').classList.remove('selected');
+            this.classList.add('selected');
+            this.closest('.select').querySelector('.select-trigger span').textContent = this.textContent;
+        }
+    })
+}
 
