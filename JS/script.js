88cf68a8712abcf1a93c8b9785c1c14eb7989e2a
Danilo Santrac <81857475+sane96@users.noreply.github.com>
2023-04-17 19:59:26 +0200

    Big changes

--- JS/script.js
+++ JS/script.js
@@ -1,118 +1,78 @@
 'use strict';
 
 const contentElements = [...document.querySelector('.container-content').children];
-const filterIcons = [...document.querySelectorAll('.icon-filters')];
-const dropdownFilters = [...document.querySelectorAll('.dropdown-filters')];
-const sectionLanding = document.getElementById('section-landing');
-const sectionHistory = document.getElementById('section-history');
-const sectionResults = document.getElementById('section-results');
-const sectionNews = document.getElementById('section-news');
-const sectionContact = document.getElementById('section-contact');
-const navbar = document.getElementById('navbar');
+const filterBtn = document.querySelector('.filters-btn');
+const dropdownFilters = document.querySelector('.dropdown-filters');
+const navUl = document.getElementById('navbar');
+const navBtns = [...document.querySelectorAll('.btn-nav')];
+const competitionSingles = [...document.querySelectorAll('.competition-table-single')];
+const navScroll = document.querySelector('.nav-scroll');
+const tabBtnContainer = document.querySelector('.tab-container-btn');
+const contentCarousel = document.querySelector('.content-carousel');
 
-// const pageId = document.body.id;
-// let verificationObj = {
-//     home: true,
-//     news: true, 
-//     results: true, 
-//     administration: true,
-//     coaches: true,
-//     history: true,
-//     contact: true, 
-//     single: true,
-// };
+// Tabbed table content
 
-// // Make session
+let tabbedTable = 'competition';
+tabBtnContainer.addEventListener('click', function(e){
+    const tableId = e.target.id;
+    if(tabbedTable == tableId) return;
+    document.getElementById(`${tabbedTable}`).parentElement.classList.remove('tab-active');
+    document.getElementById(`${tableId}`).parentElement.classList.add('tab-active');
 
-// if(!sessionStorage.initObj) {
-//     sessionStorage.setItem('initObj', JSON.stringify(verificationObj));
-// }
+    document.getElementById(`${tabbedTable}-table`).classList.remove('results-table-active');
+    document.getElementById(`${tableId}-table`).classList.add('results-table-active');
 
+    let openedSingle = competitionSingles.find(s => s.lastElementChild.classList.contains('competition-table-dropdown-visible'));
+    if(openedSingle) openedSingle.lastElementChild.classList.remove('competition-table-dropdown-visible');
 
-// const verificationId = JSON.parse(sessionStorage.getItem('initObj'))[pageId];
+    dropdownFilters.classList.remove('dropdown-filters-active');
+    filterBtn.classList.remove('filters-btn-active');
 
-
-
-// if(verificationId) {
-//     let parsedObj = JSON.parse(sessionStorage.getItem('initObj'));
-
-//     parsedObj[pageId] = false;
-//     sessionStorage.setItem('initObj', JSON.stringify(parsedObj));
-
-// Smooth scrolling
-
-navbar.addEventListener('click', function(e) {
-    e.preventDefault();
-    console.log(e.target);
-
-    sectionLanding.scrollIntoView({ behavior: 'smooth' });
+    tabbedTable = tableId;
 })
 
+// // Navigation scroll 
+// const navScrolling = function(entries, observer) {
+//     const [entry] = entries;
 
-contentElements.forEach(el => el.style.setProperty ("transition", "transform 1s, opacity 1s"));
-contentElements.forEach(el => el.classList.add('element-hidden'));
+//     if(!entry.isIntersecting) return;
 
-const elementTransition = window.getComputedStyle(contentElements[0]).transition;
+//     const elementIndex = contentElements.findIndex(ele => ele.id == entry.target.id);
 
-if(elementTransition === 'all 0s ease 0s') {
-        contentElements.forEach(el => el.style.transition = "transform 1s, opacity 1s") 
-}
-    else {
-        contentElements.forEach(el => el.style.transition = `transform 1s, opacity 1s, ${elementTransition}`);    
-}
-
-const startingCoords = contentElements.map(ele => ele.getBoundingClientRect().top);
-
-
-const initLoad = function() {
-    
-    startingCoords.forEach(ele => {
-
-    // startingCoords = ele.getBoundingClientRect().top * 1.15;
-    if(ele > window.innerHeight) return;
-
-    contentElements[startingCoords.indexOf(ele)].classList.remove('element-hidden');
-        
-});
-};
-
-initLoad();
-
-const revealContent = function(entries, observer) {
-    const [entry] = entries;
-
-    if(!entry.isIntersecting) return;
-
-    entry.target.classList.remove('element-hidden');
-    observer.unobserve(entry.target);
-};
-
-const contentObserver = new IntersectionObserver(revealContent, {
-    root: null,
-    threshold: .20,   
-});
-
-contentElements.forEach(function(ele) {
-    contentObserver.observe(ele);
-       
-});
+//     navScroll.style.left = `${elementIndex*120}px`;
 // };
 
-// Open filter dropdown
-filterIcons.forEach(icon => icon.addEventListener('click', function (e) {
-    if(e.target.parentElement.classList.contains('icon-filters')) {
-        e.target.parentElement.nextElementSibling.classList.toggle('dropdown-filters-active');
-        e.target.parentElement.classList.toggle('icon-filters-active');
-    }
 
-    if(e.target.parentElement.classList.contains('filters-row')) {
-        e.target.nextElementSibling.classList.toggle('dropdown-filters-active');
-        e.target.classList.toggle('icon-filters-active');
+// Navigation scrolling
+navUl.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    if(e.target.classList.contains('btn-nav')) {
+        const id = e.target.getAttribute('href');
+        
+        let btnIndex = navBtns.findIndex(btn => btn.getAttribute('href') == id);
+        contentCarousel.style.left = `-${btnIndex*100}%`;
+
+        navScroll.style.left = `${btnIndex*120-2}px`;
     }
-}))
+}); 
+
+
+// Competition single dropdown
+competitionSingles.forEach(single => single.addEventListener('click', function(e) {
+    let openedSingle = competitionSingles.find(s => s.lastElementChild.classList.contains('competition-table-dropdown-visible'));
+    e.currentTarget.lastElementChild.classList.toggle('competition-table-dropdown-visible');
+    if(openedSingle) openedSingle.lastElementChild.classList.remove('competition-table-dropdown-visible');
+}));
+
+// Open filter dropdown
+filterBtn.addEventListener('click', function (e) {
+
+    dropdownFilters.classList.toggle('dropdown-filters-active');
+    filterBtn.classList.toggle('filters-btn-active');
+});
 
 // Open select filter
-
 for (const dropdown of document.querySelectorAll(".select-wrapper")) {
     dropdown.addEventListener('click', function() {
         this.querySelector('.select').classList.toggle('open');
@@ -137,5 +97,51 @@ for (const option of document.querySelectorAll(".option")) {
             this.closest('.select').querySelector('.select-trigger span').textContent = this.textContent;
         }
     })
-}
+};
+
+
+
+// // Hide and reveal elements
+// contentElements.forEach(el => el.style.setProperty ("transition", "transform 1s, opacity 1s"));
+// contentElements.forEach(el => el.classList.add('element-hidden'));
+
+// const elementTransition = window.getComputedStyle(contentElements[0]).transition;
+
+// if(elementTransition === 'all 0s ease 0s') {
+//         contentElements.forEach(el => el.style.transition = "transform 1s, opacity 1s") 
+// } else {
+//         contentElements.forEach(el => el.style.transition = `transform 1s, opacity 1s, ${elementTransition}`);    
+// }
+
+// const startingCoords = contentElements.map(ele => ele.getBoundingClientRect().top);
+
+
+// const initLoad = function() {
+//     startingCoords.forEach(ele => {
+//     if(ele > window.innerHeight) return;
+
+//     contentElements[startingCoords.indexOf(ele)].classList.remove('element-hidden');
+//     })
+// };
+
+// initLoad();
+
+// const revealContent = function(entries, observer) {
+//     const [entry] = entries;
+
+//     if(!entry.isIntersecting) return;
+
+//     // entry.target.classList.remove('element-hidden');
+//     observer.unobserve(entry.target);
+// };
+
+// const contentObserver = new IntersectionObserver(revealContent, {
+//     root: null,
+//     threshold: .20,   
+// });
+
+// contentElements.forEach(function(ele) {
+//     contentObserver.observe(ele);
+// });
+
 
